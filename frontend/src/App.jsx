d0a77054c62a0222903e49f4a1d6deabc4a84ea2
Leevi Pohja <124263697+pohjal@users.noreply.github.com>
2024-09-20 20:47:02 +0300

    react app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        moi
+      </div>
+
+    </>
+  )
+}
+
+export default App
